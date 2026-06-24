9dbf08b4f700472f3c1600aed63ac050f4b5187b
Mrx™ <itzrakib34@gmail.com>
2026-06-24 13:50:23 +0600

    fix: switch to webpack, fix image config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "placehold.co" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+    ],
+  },
 };
-
 export default nextConfig;
